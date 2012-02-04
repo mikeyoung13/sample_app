8166e0246cd3be03658fcc81b2d9849c62ed0255
Michael Young <mikeyoung13@gmail.com>
2012-02-04 10:10:25 -0500

    fix mistake on error page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
         redirect_to @user
       else
         @title = "Sign up"
-        @user.name = "you suck"
         @user.password = nil
         @user.password_confirmation = nil
         render 'new'
