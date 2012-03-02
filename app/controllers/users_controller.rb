1e7ece4c2751924cc7d83e7e970c527c322932e7
Michael Young <mikeyoung13@gmail.com>
2012-03-01 19:51:18 -0500

    require signing and right user for edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -25,12 +28,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -38,6 +41,17 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
-  end    
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end 
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end   
   
 end
