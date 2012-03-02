61b4df00b2e1e33fed8bccfabba47b108f67e23d
Michael Young <mikeyoung13@gmail.com>
2012-03-01 22:33:22 -0500

    added delete feature for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def index
       @title = "All users"
@@ -48,6 +49,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
   private
   
     def authenticate
@@ -58,5 +65,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end   
-  
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
 end
