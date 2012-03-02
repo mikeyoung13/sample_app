15204f6dbc22b4d02fbf56ea99e4dc63fe5fe6a3
Michael Young <mikeyoung13@gmail.com>
2012-03-01 20:09:37 -0500

    deep linking after signing (friendly forwarding)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
 
     user = User.authenticate(params[:session][:email],
-    params[:session][:password])
+                             params[:session][:password])
     if user.nil?
       flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     else
       # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
 
   end
